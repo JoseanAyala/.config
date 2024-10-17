7c4e550cad119b39272b74ddbe080343c07c7216
Josean Ayala <36864389+JoseanAyala@users.noreply.github.com>
2024-10-17 15:36:50 -0400

    added new theme

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -5,7 +5,7 @@ return {
     priority = 1000,
     init = function()
       require("cyberdream").setup({
-        transparent = true,
+        transparent = false,
         italic_comments = true,
         hide_fillchars = true,
         borderless_telescope = false,
@@ -13,9 +13,19 @@ return {
     end,
   },
   {
+    "sainnhe/sonokai",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.sonokai_enable_italic = true
+      vim.g.sonokai_transparent_background = 2
+      vim.g.sonokai_style = "atlantis"
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "cyberdream",
+      colorscheme = "sonokai",
     },
   },
 }
